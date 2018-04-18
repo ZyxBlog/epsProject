626391b949b343bfa68e25f819576247f98da4c2
zyx <609629767@qq.com>
2018-04-18 22:43:31 +0800

    fix

--- apps/users/views.py
+++ apps/users/views.py
@@ -33,7 +33,8 @@ class LoginView(View):
             user = authenticate(username=user_name, password=pass_word)
             if user is not None:
                 login(request, user)
-                return render(request, 'index.html')
+                factory = FactoryInformation.objects.all()
+                return render(request, 'index.html', {'factory': factory})
             else:
                 return render(request, 'login.html', {'msg': u'用户名或密码错误!'})
         else:
@@ -44,6 +45,21 @@ class IndexView(View):
     def get(self, request):
         factory = FactoryInformation.objects.all()
         return render(request, 'index.html', {'factory': factory})
+
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                factory = FactoryInformation.objects.all()
+                return render(request, 'index.html', {'factory': factory})
+            else:
+                return render(request, 'login.html', {'msg': u'用户名或密码错误!'})
+        else:
+            return render(request, 'login.html', {'login_form': login_form})
 
 
 class DetailView(View):
--- static/js/ordersInfo.js
+++ static/js/ordersInfo.js
@@ -5,7 +5,9 @@ $(function() {
     // }
 
     const a = $("tbody").children();
-    for(let i = 0; i < a.length; i++) {
+
+
+    for (let i = 0; i < a.length; i++) {
         if (i % 4 == 0) {
             a.eq(i).addClass('warning')
         } else if (i % 4 == 1) {
@@ -16,4 +18,12 @@ $(function() {
             a.eq(i).addClass('success')
         }
     }
+
+    if ($('tbody').eq(0).children() && $('tbody').eq(0).children().length > 0) {
+        $("#nolist").hide();
+    } else {
+        $("#nolist").show();
+        $("table").eq(0).hide();
+    }
+
 })
