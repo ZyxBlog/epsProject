b9731ad47f05a6ba0c6e94c82bb493132995ac33
zyx <609629767@qq.com>
2018-04-23 09:27:50 +0800

    fix

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 import sys
 import django
+
 django.setup()
 
+
 if __name__ == "__main__":
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epsManage.settings")
 
     from django.core.management import execute_from_command_line
